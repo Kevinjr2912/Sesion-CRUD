102ce21f421470d40ea6ad7535ee458eb2551419
Kevin <moralesperezkevin7@gmail.com>
2024-10-19 12:35:09 -0600

    add: Componente view-main-user en módulo login

--- sesion-crud/src/app/login/login.module.ts
+++ sesion-crud/src/app/login/login.module.ts
@@ -2,18 +2,21 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SignInComponent } from './sign-in/sign-in.component';
 import { LogInComponent } from './log-in/log-in.component';
+import { ViewMainUserComponent } from './view-main-user/view-main-user.component';
 
 @NgModule({
   declarations: [
     SignInComponent,
-    LogInComponent
+    LogInComponent,
+    ViewMainUserComponent
   ],
   imports: [
-    CommonModule
+    CommonModule 
   ],
   exports: [
     SignInComponent,
     LogInComponent
   ]
 })
+
 export class LoginModule { }
---
+++ sesion-crud/src/app/login/view-main-user/view-main-user.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-view-main-user',
+  templateUrl: './view-main-user.component.html',
+  styleUrl: './view-main-user.component.css'
+})
+export class ViewMainUserComponent {
+
+}
