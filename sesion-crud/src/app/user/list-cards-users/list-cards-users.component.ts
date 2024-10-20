8048205c9a7a0da27c8e6435c12c8a7a5be97417
Kevin <moralesperezkevin7@gmail.com>
2024-10-20 11:22:32 -0600

    add: generando el componente list-cards-user

---
+++ sesion-crud/src/app/user/list-cards-users/list-cards-users.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-list-cards-users',
+  templateUrl: './list-cards-users.component.html',
+  styleUrl: './list-cards-users.component.css'
+})
+export class ListCardsUsersComponent {
+
+}
--- sesion-crud/src/app/user/user.module.ts
+++ sesion-crud/src/app/user/user.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CardInformationUserComponent } from './card-information-user/card-information-user.component';
+import { ListCardsUsersComponent } from './list-cards-users/list-cards-users.component';
 
 @NgModule({
   declarations: [
-    CardInformationUserComponent
+    CardInformationUserComponent,
+    ListCardsUsersComponent
   ],
   imports: [
     CommonModule
