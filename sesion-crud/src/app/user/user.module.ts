603dabeab954487efcf0780f85ecb4ba3255d124
Kevin <moralesperezkevin7@gmail.com>
2024-10-20 09:53:48 -0600

    add: agregando el componente card-information-user

--- sesion-crud/src/app/user/user.module.ts
+++ sesion-crud/src/app/user/user.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { CardInformationUserComponent } from './card-information-user/card-information-user.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    CardInformationUserComponent
+  ],
   imports: [
     CommonModule
   ]
