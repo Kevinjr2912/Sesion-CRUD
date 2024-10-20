f18b239e4be141cf3d7af8b231d99d500f2ca129
Kevin <moralesperezkevin7@gmail.com>
2024-10-20 11:05:58 -0600

    add: v1 de la estructura de card-information-user

--- sesion-crud/src/app/user/card-information-user/card-information-user.component.ts
+++ sesion-crud/src/app/user/card-information-user/card-information-user.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-card-information-user',
@@ -6,5 +6,13 @@ import { Component } from '@angular/core';
   styleUrl: './card-information-user.component.css'
 })
 export class CardInformationUserComponent {
+  // Inputs para redenrizar la información de un usuario
+  @Input() id_user: number = 0;
+  @Input() name_user: string = '';
+  @Input() email_user: string = '';
+  @Input() password_user: string = '';
+  
+  // Input para renderizar las acciones
+  @Input() flag: boolean = false;
 
 }
--- sesion-crud/src/app/user/user.module.ts
+++ sesion-crud/src/app/user/user.module.ts
@@ -2,14 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CardInformationUserComponent } from './card-information-user/card-information-user.component';
 
-
-
 @NgModule({
   declarations: [
     CardInformationUserComponent
   ],
   imports: [
     CommonModule
+  ],
+  exports: [
+    CardInformationUserComponent
   ]
 })
 export class UserModule { }
